6cda1401c717a5a7922931fbde8d535ab88747c1
Shilpa Handa <78005061+shanda-matc@users.noreply.github.com>
2023-04-07 23:13:45 -0500

    Update DemoServlet.java

--- src/java112/servlet/DemoServlet.java
+++ src/java112/servlet/DemoServlet.java
@@ -6,27 +6,41 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet(
-        name = "demo",
-        urlPatterns = {"/demo"}
+        name = "madlibs",
+        urlPatterns = {"/MadLibGame"}
 )
-public class DemoServlet extends HttpServlet {
 
+public class MadLibsServlet extends HttpServlet {
     /**
-     * Handles HTTP GET requests.
+     * Handles HTTP Post requests.
      *
      * @param request  the HttpServletRequest object
      * @param response the HttpServletResponse object
      * @throws ServletException if there is a Servlet failure
      * @throws IOException      if there is an IO failure
      */
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String url = "/demo.jsp";
+        String noun = request.getParameter("noun");
+        String verb = request.getParameter("verb");
+        String adjective = request.getParameter("adjective");
+        String adverb = request.getParameter("adverb");
+        String name = request.getParameter("name");
+
+        // Set the values
+        MadLibsBean madlibsBean = new MadLibsBean();
+        madlibsBean.setNoun(noun);
+        madlibsBean.setVerb(verb);
+        madlibsBean.setAdjective(adjective);
+        madlibsBean.setAdverb(adverb);
+        madlibsBean.setName(name);
+
+        request.setAttribute("madLibsBean", madlibsBean);
+
+        // Ask the view to take care of the values
+        String url = "/madLibsResult.jsp";
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
         dispatcher.forward(request, response);
-
     }
-
-
 }
